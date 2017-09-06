d9e87017dc1146648022b3947603c22c4603d829
c375610 <con.greenwald@gmail.com>
2017-09-05 19:43:05 -0700

    initial commit

---
+++ app/src/main/java/com/example/android/boredapptest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.boredapptest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void openRestaurants(View view) {
+        Log.d("resturants", "Restairants was clicked");
+        Intent intent = new Intent(this, Main2Activity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
